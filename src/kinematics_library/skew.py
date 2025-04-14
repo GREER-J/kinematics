4aaf683fdaab1aa9e06f839be03bc77bfe48aeca
Jay <jmlgreer@gmail.com>
2025-04-14 19:52:34 +1000

    added first skew test

---
+++ src/kinematics_library/skew.py
@@ -0,0 +1,19 @@
+import sympy as sp
+
+"""
+MATLAB implementation
+function S = skew(u)
+%skew    Obtain matrix operator for left action of cross product
+%   S = skew(u)
+%   takes column vector u of length 3 and returns a matrix S,
+%   such that
+%       S*v = cross(u,v)
+%
+%   See also cross.
+
+S = [ ...
+    0,-u(3),u(2); ...
+    u(3),0,-u(1); ...
+    -u(2),u(1),0 ...
+    ];
+"""
---
+++ tests/test_skew.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def skew(u: np.ndarray) -> np.ndarray:
+    a1 = u[0, 0]
+    a2 = u[1, 0]
+    a3 = u[2, 0]
+    return np.matrix([[0, -a3, a2], [a3, 0, -a1], [-a2, a1, 0]])
+
+
+def test_skew():
+    # Given we have a vector [1, 2, 3].T
+    a1 = 1
+    a2 = 2
+    a3 = 3
+    vec = np.matrix([a1, a2, a3]).T
+    # Then the skew of that vector should be:
+    exp_skew = np.matrix([[0, -a3, a2], [a3, 0, -a1], [-a2, a1, 0]])
+    act_skew = skew(vec)
+    assert np.allclose(act_skew, exp_skew)
+
+test_skew()
