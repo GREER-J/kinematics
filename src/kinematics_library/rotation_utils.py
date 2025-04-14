230608488ea2ad0fc4db6b81cf49495ee01ea1db
Jay <jmlgreer@gmail.com>
2025-04-15 07:59:16 +1000

    added rotation util for pulling out e vecs

---
+++ src/kinematics_library/rotation_utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def get_e1_axis(R: np.ndarray) -> np.ndarray:
+    """
+    Extracts the e1 unit vector from a 3x3 rotation matrix.
+    """
+    assert R.shape == (3, 3), f"Expected a 3x3 matrix, got shape {R.shape}."
+    return np.reshape(R[:, 0], (3,1))
+
+
+def get_e2_axis(R: np.ndarray) -> np.ndarray:
+    """
+    Extracts the e2 unit vector from a 3x3 rotation matrix.
+    """
+    assert R.shape == (3, 3), f"Expected a 3x3 matrix, got shape {R.shape}."
+    return np.reshape(R[:, 1], (3,1))
+
+
+def get_e3_axis(R: np.ndarray) -> np.ndarray:
+    """
+    Extracts the e3 unit vector from a 3x3 rotation matrix.
+    """
+    assert R.shape == (3, 3), f"Expected a 3x3 matrix, got shape {R.shape}."
+    return np.reshape(R[:, 2], (3,1))
