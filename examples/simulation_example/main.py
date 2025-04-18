1953ae032e99b5f0928cbecececb25b7bef83d85
Jay <jmlgreer@gmail.com>
2025-04-18 23:04:11 +1000

    more clean / funcitonal

--- examples/simulation_example/main.py
+++ examples/simulation_example/main.py
@@ -2,48 +2,9 @@ import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from itertools import combinations
+from dataclasses import dataclass
 from kinematics_library import homogeneous as htf
 from kinematics_library.homogeneous_utils import get_basis_vectors, get_r_and_R_from_A, get_r_from_A 
-print("Hello world")
-
-
-class CameraSensor:
-    def __init__(self, E: float, N: float, bearing_deg: float):
-        self.Apn = htf.transx(N) @ htf.transy(E) @ htf.rotz(np.deg2rad(bearing_deg))  # NED
-
-    def get_dir_vec(self, rTNn: np.ndarray) -> np.ndarray:
-        rPNn = get_r_from_A(self.Apn)
-        dr = rTNn - rPNn
-        return dr / norm(dr)
-
-
-class CameraNetwork:
-    def __init__(self, cameras: list[CameraSensor]):
-        self._cams = cameras
-
-    def get_y(self, rTNn):
-        y = []
-        for cam in self._cams:
-            y.append(cam.get_dir_vec(rTNn)[0:2])
-        return y
-    
-    def process_y(self, y):
-        intersection_points = []
-        for k, val in enumerate(combinations(y, 2)):
-            y1, y2 = val
-            # Construct homogeneous lines from camera positions and direction vectors
-            h_c1 = get_homogeneous_line_from_point_and_direction(rC1Nn, y1)
-            h_c2 = get_homogeneous_line_from_point_and_direction(rC2Nn, y2)
-
-            # Calculate intersection point with the cross product
-            p = np.cross(h_c1, h_c2)
-
-            # Normalize if the third coordinate is not zero
-            if p[2] != 0:
-                p = p / p[2]
-
-            intersection_points.append(p)
-        return intersection_points
 
 
 def get_homogeneous_line_from_point_and_direction(point, direction):
@@ -59,6 +20,80 @@ def get_homogeneous_line_from_point_and_direction(point, direction):
     # The line is the cross product of the point and the direction
     line = np.cross(point_h, direction_h)
     return line
+
+
+@dataclass
+class CameraDirectionMeasurement:
+    base_point: np.ndarray
+    dir_vec: np.ndarray
+
+
+class CameraSensor:
+    def __init__(self, E: float, N: float, bearing_deg: float):
+        self.Apn = htf.transx(N) @ htf.transy(E) @ htf.rotz(np.deg2rad(bearing_deg))  # NED
+        self.false_target_per_timestep = 10
+
+    def get_dir_vec(self, rTNn: np.ndarray) -> np.ndarray:
+        rPNn = get_r_from_A(self.Apn)
+        dr = rTNn - rPNn
+        return dr / norm(dr)
+
+    def get_false_targets(self, N: int):
+        false_targets = []
+        for _ in range(N):
+            A = self.Apn@htf.roty(np.deg2rad(np.random.uniform(-45, 45))) @ htf.rotz(np.deg2rad(np.random.uniform(-45, 45))) @ htf.transx(10)
+            r = get_r_from_A(A)
+            false_targets.append(r)
+        return false_targets
+
+    def get_detections(self, rTNn) -> list[CameraDirectionMeasurement]:
+        measurements = []
+
+        rPNn = get_r_from_A(self.Apn)
+        measurements.append(CameraDirectionMeasurement(rPNn, self.get_dir_vec(rTNn)))
+
+        for false_target in self.get_false_targets(self.false_target_per_timestep):
+            measurements.append(CameraDirectionMeasurement(rPNn, self.get_dir_vec(false_target)))
+        return measurements
+    
+    def is_point_in_vis(self, r_vec) -> bool:
+        # Rotate into maximal FOV limits and check e2 to see if it's positive or negative
+        # Or just take angle via dot product and directly check FOV limits
+        pass
+
+
+class CameraNetwork:
+    def __init__(self, cameras: list[CameraSensor]):
+        self._cams = cameras
+
+    def get_y(self, rTNn) -> list[CameraDirectionMeasurement]:
+        y = []
+        for cam in self._cams:
+            y.extend(cam.get_detections(rTNn))
+        return y
+
+    def process_y(self, y: list[CameraDirectionMeasurement]):
+        intersection_points = []
+        for k, val in enumerate(combinations(y, 2)):
+            y1, y2 = val
+            # Construct homogeneous lines from camera positions and direction vectors
+            h_c1 = get_homogeneous_line_from_point_and_direction(y1.base_point, y1.dir_vec)
+            h_c2 = get_homogeneous_line_from_point_and_direction(y2.base_point, y2.dir_vec)
+
+            # Calculate intersection point with the cross product
+            p = np.cross(h_c1, h_c2)
+
+            # Normalize if the third coordinate is not zero
+            if p[2] != 0:
+                p = p / p[2]
+
+            # Can the point be seen by the cams?
+
+            intersection_points.append(p[0:2])
+        return intersection_points
+
+    def get_z(self, rTNn):
+        return net.process_y(net.get_y(rTNn))
 
 
 # Camera 1
@@ -78,18 +113,16 @@ net = CameraNetwork([cam_1, cam_2])
 
 target_E = 1000
 
-# Example usage
-es = []
-ns = []
+zs = []
 
 for k, target_N in enumerate(np.linspace(0, 500, 100)):
     rTNn = np.matrix([[target_N], [target_E], [200]])
-    # Assume rC1Nn and rC2Nn are 3x1 numpy arrays representing camera positions
-    y = net.get_y(rTNn)
-    z = net.process_y(y)
+    z = net.get_z(rTNn)
 
-    es.append(z[0][1])
-    ns.append(z[0][0])
+    zs.append(z)
+
+    if k > 1:
+        break
 
 
 # Create a 2D plot
@@ -148,7 +181,17 @@ ax.quiver(
     # Target
 ax.plot(1000 * np.ones((100, 1)), np.linspace(0, 500, 100), '--', label='Target Position')
 
-ax.plot(es, ns, 'r--', label="Projected position?")
+for z in zs:
+    ns = []
+    es = []
+    for n in z:
+        ns.append(float(n[0]))
+        es.append(float(n[1]))
+
+    ax.scatter(es, ns)
+
+ax.set_xlim(-500, 1500)
+ax.set_ylim(-500, 1500)
 
 # Set labels and title
 ax.set_xlabel('East [m]')
@@ -164,4 +207,3 @@ ax.legend()
 
 # Show the plot
 plt.show()
-
